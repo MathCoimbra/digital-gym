86d953cb3b127954c171c85e700207569e9b014d
MathCoimbra <matheuscoimbra35@gmail.com>
2022-11-30 16:09:47 -0300

    feat: add validations on fields from Avaliacao Fisica

--- src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import me.dio.academia.digital.entity.Aluno;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,8 +14,10 @@ public class AvaliacaoFisicaForm {
 
   private Long alunoId;
 
+  @NotEmpty(message = "O campo altura está pendente, por favor preencher")
   private double peso;
 
+  @NotEmpty(message = "O campo altura está pendente, por favor preencher")
   private double altura;
 
   private double imc;
