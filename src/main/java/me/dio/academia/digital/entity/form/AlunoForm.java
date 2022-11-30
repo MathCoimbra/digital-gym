d86e51f2c4a9f9839adcbdaab3bac380964bf29f
MathCoimbra <matheuscoimbra35@gmail.com>
2022-11-30 16:09:33 -0300

    feat: add validations on fields from Aluno

--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -3,7 +3,9 @@ package me.dio.academia.digital.entity.form;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
@@ -11,11 +13,19 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class AlunoForm {
 
+  @NotEmpty(message = "O campo nome está pendente, por favor preencher")
+  @Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres")
   private String nome;
 
+  @NotEmpty(message = "O campo cpf está pendente, por favor preencher")
+  @CPF(message = "CPF '${validatedValue}' é inválido")
   private String cpf;
 
+  @NotEmpty(message = "O campo bairro está pendente, por favor preencher")
+  @Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres")
   private String bairro;
 
+  @NotNull(message = "O campo data de nascimento está pendente, por favor preencher")
+  @Past(message = "Data '${validatedValue}' é inválida")
   private LocalDate dataDeNascimento;
 }
